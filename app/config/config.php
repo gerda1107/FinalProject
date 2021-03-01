e74399697756ab872b1776c76fc82ffa902d7309
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-01 14:48:40 +0200

    created config file w/site details and db name

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'gym');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://localhost/FinalProject');
+define('SITENAME', 'THE GYM');
