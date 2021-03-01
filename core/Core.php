f26e0785277e875d13a5b46149de7803521646ee
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-01 16:10:14 +0200

    added fn to getUrl in order to be able to load requested pages

--- core/Core.php
+++ core/Core.php
@@ -2,10 +2,26 @@
 
 namespace core;
 
+use app\controllers\Pages;
+
 class Core
 {
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+    
     public function __construct()
     { 
         echo "hello";
+    }
+
+    public function getUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
