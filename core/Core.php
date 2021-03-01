1dfa925130e73d8fb304abb9ad248ff36e120c39
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-01 16:13:47 +0200

    added functionality to test url arr element to load correct requested controller and method

--- core/Core.php
+++ core/Core.php
@@ -12,9 +12,35 @@ class Core
     
     public function __construct()
     { 
-        echo "hello";
-    }
+        $url = $this->getUrl();
 
+        if (isset($url[0])) {
+            if (file_exists('../app/controllers/' . ucfirst($url[0]) . '.php')) {
+                $this->currentController = ucfirst($url[0]);
+                unset($url[0]);
+            }
+        }
+
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        if ($this->currentController === "Pages") {
+            $this->currentController = new Pages;
+        }
+
+        if (isset($url[1])) {
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        if (isset($url[2])) {
+            $this->params = $url ? array_values($url) : [];
+        }
+
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+    }
+    
     public function getUrl()
     {
         if (isset($_GET['url'])) {
