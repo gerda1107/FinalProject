97b41684eb193a8816fb44055be8a4a72eb5b80c
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-01 19:45:51 +0200

    adj navbar.php -> header.php, header css per requirements

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,18 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<header class="stick d-flex justify-content-center">
+    <nav class="d-flex justify-content-between navRes">
 
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo SITENAME; ?></title>
-    <script src="https://kit.fontawesome.com/42762d49f1.js" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
-    <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-    
-    <script src="<?php echo URLROOT; ?>/js/script.js" defer></script>
-</head>
+        <div class="d-flex justify-content-between align-items-center">
+            <a href=" <?php echo URLROOT; ?>" class="nav-link">TITULINIS</a>
+            <a class="nav-link" href="#">APIE MUS</a>
+        </div>
 
-<body>
-<?php require APPROOT . '/views/inc/navbar.php'; ?>
+        <div class="d-flex justify-content-between align-items-center">
+            <a class="nav-link" href="#">PRISIJUNGTI</a>
+            <a class="nav-link" href="#">REGISTRUOTIS</a>
+        </div>
+    </nav>
+</header>
