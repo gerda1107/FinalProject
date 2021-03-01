69fe3f668e710638c107edc9ff110b795bc6f890
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-01 22:06:55 +0200

    added main bg img and services according to requirements to index.php view

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,27 @@
 <?php require APPROOT . '/views/inc/head.php'; ?>
 
-<h1>Pages</h1>
+<main>
+    <div class="bgImg"></div>
+
+    <section class="d-flex justify-content-center">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="services">
+                <img src="public/img/img1.jpg" alt="treniruotes">
+                <h5>TRENIRUOTĖS</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ducimus aperiam officiis rerum.</p>
+            </div>
+            <div class="services">
+                <img src="public/img/img2.jpg" alt="baseinas">
+                <h5>BASEINAS</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab fugit harum sint officiis ullam laborum.</p>
+            </div>
+            <div class="services">
+                <img src="public/img/img3.jpg" alt="mityba">
+                <h5>MITYBOS PLANAI</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ullam laborum assumenda dicta.</p>
+            </div>
+        </div>
+    </section>
+</main>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
