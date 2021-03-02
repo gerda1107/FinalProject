29a101926e03712333b6e00fd55e5387f1ec19c4
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 20:27:33 +0200

    index and addComment fn added, input result encoded to json

--- app/controllers/Comments.php
+++ app/controllers/Comments.php
@@ -19,7 +19,59 @@ class Comments extends Controller
 
     public function index()
     {
-        $data = [];
+        if(isset($_SESSION['user_id'])) {
+          $data = [
+            'username' => $this->commentModel->getUsername($_SESSION['user_id'])
+            ];  
+        } else {
+             $data = [];
+        }
+        
         $this->view('comments/feedback', $data);
+    }
+
+    public function addComment()
+    {
+        if (ifRequestIsPost()) {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $id = $_SESSION['user_id'];
+
+            $data = [
+                'user_id' => $id,
+                'username' => trim($_POST['userName']),
+                'commentBody' => trim($_POST['commentBody']),
+
+                'errors' => [
+                    'usernameErr' => '',
+                    'commentBodyErr' => '',
+                ],
+            ];
+
+            $data['errors']['usernameErr'] = $this->vld->validateName($data['username']);
+            $data['errors']['commentBodyErr'] = $this->vld->validateComment($data['commentBody']);
+
+            if ($this->vld->ifEmptyArr($data['errors'])) {
+                $commentData = [
+                    'user_id' => $data['user_id'],
+                    'commentBody' => $data['commentBody'],
+                ];
+
+                if ($this->commentModel->addComment($commentData)) {
+                    $result['success'] = "Komentaras sėkmingai pridėtas.";
+                } else {
+                    $result['errors'] = "Bandykite dar kartą.";
+                }
+            } else {
+                $result['errors'] = $data['errors'];
+            }
+
+        } else {
+            $result['error'] = 'not allowed';
+            redirect('/comments');
+        }
+
+        echo json_encode($result);
+        die();  
     }
 }
