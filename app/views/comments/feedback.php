09d8da2b320bbbf9dc336ee563d07330f42e840f
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 20:28:19 +0200

    preventing default, functionality in progress to add comments w/js

--- app/views/comments/feedback.php
+++ app/views/comments/feedback.php
@@ -1,4 +1,7 @@
-<?php require APPROOT . '/views/inc/head.php'; ?>
+<?php require APPROOT . '/views/inc/head.php';
+print_r($_POST);
+print_r($data);
+?>
 
 <h2>ATSILIEPIMAI</h2>
 <section>
@@ -25,25 +28,37 @@
     </table>
 </section>
 
- <?php if (!ifUserIsLoggedIn()) : ?>
+<?php if (!ifUserIsLoggedIn()) : ?>
     <div>
         <span>Norite parašyti komentarą? <a class="" href="<?php echo URLROOT; ?>/users/register">Užsiregistruokite</a>!</span>
     </div>
-<?php else :?>
-<h4>Rašyti komentarą:</h4>
-<section class="d-flex justify-content-center align-items-center">
-    <form class="feedbackFormCont" method="POST">
-        <div class="container">
-            <div class="mb-3">
-                <input type="email" class="form-control" name="userName" id="userName" placeholder="Jūsų vardas..">
+<?php else : ?>
+    <h4>Rašyti komentarą:</h4>
+    <section class="d-flex justify-content-center align-items-center">
+        <form id="feedbackFormCont" action='' method='post'>
+            <div class="container">
+                <div class="mb-3">
+                    <input type="text" class="form-control" name="userName" id="userName" placeholder="Jūsų vardas.." value="<?php echo $data['username']->name . " " . $data['username']->lastname; ?>">
+                </div>
+                <div class="mb-3">
+                    <textarea type="text" class="form-control" name="commentBody" id="commentBody" placeholder="Jūsų komentaras.." rows="7"></textarea>
+                </div>
+                <button type="submit" class="btn btn-dark mt-3" id="commentBtn">KOMENTUOTI</button>
             </div>
-            <div class="mb-3">
-                <textarea type="text" class="form-control" name="commentField" id="commentField" placeholder="Jūsų komentaras.." rows="7"></textarea>
-            </div>
-            <button type="submit" class="btn btn-dark mt-3">KOMENTUOTI</button>
-        </div>
-    </form>
-</section>
+        </form>
+    </section>
+
+    <script>
+        const addCommentForm = document.getElementById('feedbackFormCont');
+        const submitComment = document.getElementById('commentBtn');
+
+        addCommentForm.addEventListener('submit', addComment);
+
+        function addComment(event) {
+            event.preventDefault();
+            console.log('add Comment pls');
+        }
+    </script>
 <?php endif; ?>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
