8fd8e922100ac5aa838f478c25c1e1d3a7b1fe18
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 11:21:28 +0200

    Validation added to validate name and lastname

--- core/Validation.php
+++ core/Validation.php
@@ -4,5 +4,22 @@ namespace core;
 
 class Validation
 {
-    
+    public function validateName($field)
+    {
+        if (empty($field)) return 'Please enter your Name.';
+
+        if (!preg_match("/^[a-z ,.'-]+$/i", $field)) return 'Name must only contain name Chars.';
+
+        return '';
+    }
+
+    public function validateLastame($field)
+    {
+        if (empty($field)) return 'Please enter your Lastname.';
+
+        if (!preg_match("/^[a-z ,.'-]+$/i", $field)) return 'Lastname must only contain name Chars.';
+
+        return '';
+    }
+
 }
