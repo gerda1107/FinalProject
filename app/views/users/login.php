806708723a9489cf876505ce7c7d6b9bbd635e22
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 10:55:04 +0200

    login.php view styled

---
+++ app/views/users/login.php
@@ -0,0 +1,29 @@
+<?php require APPROOT . '/views/inc/head.php'; ?>
+<section class="d-flex justify-content-center align-items-center loginBg p-4">
+
+    <div class="container">
+        <h2 class="text-center mb-5"><strong>PRISIJUNGTI</strong></h2>
+
+        <form action='' method='post' class="p-2">
+            <div class="row mb-3">
+                <label for='email' class="col-sm-2 col-form-label">EL. PAŠTAS</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name='email' id='email' placeholder="ex. johnwick@gmail.com" value="">
+                    <small class="error-alert"></small>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <label for='passwordLog' class="col-sm-2 col-form-label">SLAPTAŽODIS</label>
+                <div class="col-sm-10">
+                    <input class="form-control" name='password' id='password' value="" type="password">
+                    <small class="error-alert"></small>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <button type='submit' name="btnName" class="btn btn-dark mt-5" value="login">PRISIJUNGTI</button>
+            </div>
+
+        </form>
+    </div>
+</section>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
