f27c1b1405144f9f75fcea26c047dd3349294540
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 20:19:52 +0200

    addComment and getUsername methods added to Comment.php model

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -12,4 +12,30 @@ class Comment
     {
         $this->db = new Database;
     }
+
+    public function addComment($data)
+    {
+        $this->db->query('INSERT INTO comments (user_id, comment_body) VALUES (:user_id, :comment_body)');
+
+        $this->db->bind(':user_id', $data['user_id']);
+        $this->db->bind(':comment_body', $data['commentBody']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getUsername($id)
+    {
+        $this->db->query("SELECT name, lastname FROM users WHERE user_id = :id");
+
+        $this->db->bind(':id', $id);
+
+        $row = $this->db->singleRow();
+
+        if ($this->db->rowCount() > 0) return $row;
+        return false;
+    }
 }
