418f1b8e72e9b8b008e3fc8369f5ceea056d4f0a
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 11:28:03 +0200

    core Database fn added, User.php model register fn to be able to register a user

--- app/models/User.php
+++ app/models/User.php
@@ -2,7 +2,32 @@
 
 namespace app\models;
 
+use core\Database;
+
 class User
 {
-    
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function register($data)
+    {
+        $this->db->query("INSERT INTO users (`name`, `lastname`, `email`, `phone`, `address`, `password`) VALUES (:name, :lastname, :email, :phone, :address, :password)");
+
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':lastname', $data['lastname']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':phone', $data['phone']);
+        $this->db->bind(':address', $data['address']);
+        $this->db->bind(':password', $data['password']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
