379965403f3d868e337eb408bf416a2bfb3ce9d0
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 13:17:52 +0200

    change header view depending if user is logged in or not

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -3,12 +3,16 @@
 
         <div class="d-flex justify-content-between align-items-center">
             <a href="<?php echo URLROOT; ?>" class="nav-link">TITULINIS</a>
-            <a class="nav-link" href="<?php echo URLROOT; ?>">APIE MUS</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>">ATSILIEPIMAI</a>
         </div>
-
+        
         <div class="d-flex justify-content-between align-items-center">
+            <?php if (!ifUserIsLoggedIn()) : ?>
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">PRISIJUNGTI</a>
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">REGISTRUOTIS</a>
+            <?php else : ?>
+            <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">ATSIJUNGTI</a>
+            <?php endif; ?>
         </div>
     </nav>
 </header>
