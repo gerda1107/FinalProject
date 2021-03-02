8479901f5000466cced77e015ed198dcc3e5af6c
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 10:57:43 +0200

    added correct href to open requested pages

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -2,13 +2,13 @@
     <nav class="d-flex justify-content-between navRes">
 
         <div class="d-flex justify-content-between align-items-center">
-            <a href=" <?php echo URLROOT; ?>" class="nav-link">TITULINIS</a>
-            <a class="nav-link" href="#">APIE MUS</a>
+            <a href="<?php echo URLROOT; ?>" class="nav-link">TITULINIS</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>">APIE MUS</a>
         </div>
 
         <div class="d-flex justify-content-between align-items-center">
-            <a class="nav-link" href="#">PRISIJUNGTI</a>
-            <a class="nav-link" href="#">REGISTRUOTIS</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">PRISIJUNGTI</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">REGISTRUOTIS</a>
         </div>
     </nav>
 </header>
