741c17a8c86afa67dfab6e0f6b211194814d308f
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-02 10:56:05 +0200

    Users.php controller to load login and register views to try

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -16,4 +16,16 @@ class Users extends Controller
         $this->userModel = new User;
         $this->vld = new Validation;
     }
+
+    public function register()
+    {
+        $data = [];
+        $this->view('users/register', $data);
+    }
+
+    public function login()
+    {
+        $data = [];
+        $this->view('users/login', $data);
+    }
 }
